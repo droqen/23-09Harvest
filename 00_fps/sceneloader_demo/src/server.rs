360641a5ab1d179ae860cdd4949b51f4979aa760
Alexander Martin <hi@droqen.com>
2023-09-05 18:22:53 -0400

    deleted old scene, replaced with dan's buildings

---
+++ 00_fps/sceneloader_demo/src/server.rs
@@ -0,0 +1,79 @@
+use ambient_api::{
+    core::{
+        app::components::main_scene,
+        camera::{
+            components::aspect_ratio_from_window,
+            concepts::make_perspective_infinite_reverse_camera,
+        },
+        prefab::components::prefab_from_url,
+        primitives::components::{cube, quad},
+        transform::{
+            components::{lookat_target, rotation, scale, translation},
+            concepts::make_transformable,
+        },
+    },
+    prelude::*,
+};
+
+mod sceneloader;
+
+#[main]
+pub fn main() {
+    let main_camera_ent = Entity::new()
+        .with_merge(make_perspective_infinite_reverse_camera())
+        .with(aspect_ratio_from_window(), EntityId::resources())
+        .with(main_scene(), ())
+        .with(translation(), vec3(9.5, -65.0, 18.0))
+        .with(lookat_target(), vec3(0., 0., 0.))
+        .spawn();
+
+    Entity::new()
+        .with_merge(make_transformable())
+        .with(quad(), ())
+        .with(scale(), Vec3::splat(100.))
+        .spawn();
+
+    let nodes = sceneloader::test_get_nodes();
+
+    sceneloader::debug_nodes(&nodes);
+
+    for (_key, node) in nodes {
+        let node_pos: Option<Vec3> = node.pos;
+        let node_rot: Option<Quat> = node.rot;
+        match node.name.as_str() {
+            "cube1" | "cube2" => {
+                println!(
+                    "Spawn one cube @ pos {:?} rot {:?}",
+                    node_pos.unwrap(),
+                    node_rot.unwrap()
+                );
+                Entity::new()
+                    .with_merge(make_transformable())
+                    .with(cube(), ())
+                    .with(translation(), node_pos.unwrap())
+                    .with(rotation(), node_rot.unwrap())
+                    .spawn();
+            }
+            "camera" => {
+                println!(
+                    "Yes: Found camera! @ pos {:?} rot {:?}",
+                    node_pos.unwrap(),
+                    node_rot.unwrap()
+                );
+                entity::set_component(main_camera_ent, translation(), node_pos.unwrap());
+                entity::set_component(main_camera_ent, rotation(), node_rot.unwrap());
+            }
+            _ => {
+                if let Some(path) = node.path {
+                    Entity::new()
+                        .with_merge(make_transformable())
+                        // .with_default(cube())
+                        .with(translation(), node_pos.unwrap())
+                        .with(rotation(), node_rot.unwrap())
+                        .with(prefab_from_url(), packages::this::assets::url(&path))
+                        .spawn();
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,20 @@ fresh out of the maze landing. until september's harvest moon.
 
 # Sep 5
 - [...] Just get a scene working using FPS assets
+
+        Importing the .fbx was not working right; it had its
+        rotation pivot point somewhere very far away! I re-
+        exported it in blender.
+
+        Also, I can't figure out how to import textures or
+        normals or anything like that?
+
+        ...
+
+        OMG lol is this some sort of prank? the .glb file in
+        bldg_maze/assets imports as a tiny tiny speck hahahahaha
+
+- [x] Used Dan Hunter's assets instead, and my Godot scene loader.
+        
+        Worked *great*!!!
+        See screenshots d00 and d01 in _documentation.
