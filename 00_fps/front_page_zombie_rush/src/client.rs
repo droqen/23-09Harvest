4675c6fb2abc9762186e75c3f60e6069db487e8a
Alexander Martin <hi@droqen.com>
2023-09-07 14:08:41 -0400

    added debug key to front_page_zombie_rush

--- 00_fps/front_page_zombie_rush/src/client.rs
+++ 00_fps/front_page_zombie_rush/src/client.rs
@@ -1,4 +1,14 @@
 use ambient_api::prelude::*;
 
 #[main]
-pub fn main() {}
+pub fn main() {
+    ambient_api::core::messages::Frame::subscribe(|_| {
+        let (delta, input) = input::get_delta();
+        if delta.keys.contains(&KeyCode::Q) {
+            println!("Q - on");
+        }
+        if delta.keys_released.contains(&KeyCode::Q) {
+            println!("Q - off");
+        }
+    });
+}
